c75bc29f067dc242e4d2cc78ebeccb7a2f6125cc
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-03-29 12:01:49 -0300

    ok

--- Curso_Python/aula004_desafio003.py
+++ Curso_Python/aula004_desafio003.py
@@ -1,2 +1,6 @@
 #Crie um script Python que leia dois números e tente mostrar a soma entre eles.
 
+número01 = input("Digite o primeiro número ")
+número02 = input("Digite o segundo número ")
+S = (número01 + número02)
+print("A soma é,", S)
