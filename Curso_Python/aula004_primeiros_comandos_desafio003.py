fe4f6331ceabd8709793e17bb4c5dba6745fc2fb
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-03-30 18:37:36 -0300

    Adicionei a aula 6

--- Curso_Python/aula004_primeiros_comandos_desafio003.py
+++ Curso_Python/aula004_primeiros_comandos_desafio003.py
@@ -2,5 +2,7 @@
 
 número01 = input("Digite o primeiro número ")
 número02 = input("Digite o segundo número ")
-S = (número01 + número02)
+S = número01 + número02
 print("A soma é,", S)
+
+#Nesse caso, o sinal de "+" não está cumprindo a função adição, e sim de concatenação. Ele une os dois números sem somar, isso acontece no python porque não usamos os tipos primitivos.
