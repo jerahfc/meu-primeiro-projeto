bd46f618e7ceafa6ba78dc8784f4ee00b41b9a5d
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-07 16:19:49 -0300

    ex004

--- Curso_Python/ex004.py
+++ Curso_Python/ex004.py
@@ -1,1 +1,4 @@
-# Faça
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele?
+
+a = input("Digite algo: ")
+print("O tipo primitivo desse valor é ", type(a))
