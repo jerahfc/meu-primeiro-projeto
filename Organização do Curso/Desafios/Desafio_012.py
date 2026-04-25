0f33df4c14fac05b1ff2da323a5fa0c562e2efd0
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-25 17:50:04 -0300

    ok

--- Organização do Curso/Desafios/Desafio_012.py
+++ Organização do Curso/Desafios/Desafio_012.py
@@ -1,7 +1,7 @@
 # Faça um algoritmo que leia o preço de um produto e mostre, seu novo preço, com 5% de desconto.
 
 preco_do_produto = float(input("Qual o preço do produto? R$"))
-desconto = (preco_do_produto*5)/100
+desconto = (preco_do_produto * 5) / 100
 preco_do_produto_com_desconto = preco_do_produto - desconto
 print(f"O produto com 5% de desconto custa R${preco_do_produto_com_desconto:.2f}")
 
