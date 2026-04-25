71facb43b9be95ce55f396f2641f5e31d67d178a
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-25 05:39:13 -0300

    fiz o desafio 8

--- Organização do Curso/Desafios/Desafio_007.py
+++ Organização do Curso/Desafios/Desafio_007.py
@@ -2,4 +2,6 @@
 
 prova1 = float(input("Digite sua nota da prova: "))
 prova2 = float(input("Digite sua outra nota: "))
-media = print(f"A sua média é: {(prova1 + prova2) / 2}")
+print(f"A sua média é: {(prova1 + prova2) / 2}")
+
+# Eu quis usar float para ter a média com casa decimal.
---
+++ Organização do Curso/Desafios/Desafio_008.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetro e milímetros.
+
+medida = int(input("Digite uma medida em metros: "))
+print(f"A sua medida em centímetros é {medida * 100}, e em milímetros é {medida * 1000} ")
