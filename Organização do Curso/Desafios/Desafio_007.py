6f0fe36abb3e91417061fa1ce6189106840e9f3d
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-25 05:28:21 -0300

    realizei o desafio 7

---
+++ Organização do Curso/Desafios/Desafio_007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+prova1 = float(input("Digite sua nota da prova: "))
+prova2 = float(input("Digite sua outra nota: "))
+media = print(f"A sua média é: {(prova1 + prova2) / 2}")
