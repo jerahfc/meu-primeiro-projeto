f2497be00955342ca6351e0af740b487bc4c934d
Jerah Fonseca Carvalho <jerah.fc@gmail.com>
2026-04-27 16:23:48 -0300

    ok

--- Organização do Curso/Desafios/Desafio_006.py
+++ Organização do Curso/Desafios/Desafio_006.py
@@ -2,3 +2,8 @@
 
 n = int(input("Digite um número: "))
 print(f"O dobro de {n} é {n*2}, o triplo é {n*3} e a raiz quadrada é {n**(1/2):.2f}")
+
+# ------------OUTRA FORMA DE FAZER--------------------------------------------------------------------------------------
+
+n = int(input("Digite um número: "))
+print(f"O dobro de {n} é {n*2}, o triplo é {n*3} e a raiz quadrada é {pow(n, (1/2)):.2f}")
